65088c7e15c111f3316604ec849343464dd0d5a9
William <w.reed87@gmail.com>
2020-04-18 19:23:25 +0000

    Item, Auction Models created

--- bidder/src/myapp/admin.py
+++ bidder/src/myapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Item # Import Item class as model
+from .models import Auction # Import Auction class as model
 
 # Register your models here.
+
+admin.site.register(Item) # Register the Item class in # admin
+admin.site.register(Auction) # Register the Auction class in # admin
--- bidder/src/myapp/models.py
+++ bidder/src/myapp/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from datetime import datetime
+from decimal import Decimal
+from django.core.validators  import MinValueValidator
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Item(models.Model):
+    itmTitle = models.CharField(max_length=60)
+    itmTimeStamp = models.DateTimeField(auto_now_add=True, blank=True)
+    itmIsNew = models.BooleanField()
+    itmDescription = models.CharField(max_length=100)
+    itmExpireDateTime = models.DateTimeField(default=timezone.now)
+    itmOwner = models.CharField(max_length=60)        
+    def __str__(self):
+        return self.name
+
+    
+class Auction(models.Model):
+    aucPrice = models.DecimalField(max_digits=6, decimal_places=2, validators=[MinValueValidator(Decimal('0.01'))]) #ensures price is higher than 0
+    aucBidder = models.CharField(max_length=60)
+    aucIsOpen = models.BooleanField(default=True)
+    aucDateTimeCountdown = models.DateTimeField()
+    aucWinner = models.CharField(max_length=60)
+    def __str__(self):
+        return self.name
